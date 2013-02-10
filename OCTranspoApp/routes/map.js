9963503a35493cbd09ac2ad63f6a4c66eb504acc
Tim Thornton <t.thornton6@gmail.com>
2013-02-09 20:21:05 -0500

    Removing old route

--- OCTranspoApp/routes/map.js
+++ OCTranspoApp/routes/map.js
@@ -3,12 +3,6 @@ exports.home = function(req, res){
   res.render('home', { title: 'OCTranspo App' })
 };
 
-// handler for form submitted from homepage
-exports.home_post_handler = function(req, res) {
-  console.log('Request for stop - ' + req.body.stopID);
-  res.send('RESPONSE_TEXT_:D');
-};
-
 // handler for getting trips
 exports.getTrips = function(req, res) {
   console.log('Incoming request for stop - ' + req.body.stopID);
