9ed63e28209370e89baef0b86d9e21ab9f3eba50
Tim Thornton <t.thornton6@gmail.com>
2013-02-11 16:50:16 -0500

    Changed map to module, changed express indentation

--- OCTranspoApp/app.js
+++ OCTranspoApp/app.js
@@ -4,28 +4,28 @@
  */
 
 var express = require('express')
-  , map = require('./routes/map')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , http = require('http');
+    , map = require('./routes/map')
+    , user = require('./routes/user')
+    , http = require('http')
+    , path = require('path')
+    , http = require('http');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 // Routes
@@ -35,5 +35,5 @@ app.post('/getSummary', map.getSummary);
 app.post('/getTrips', map.getTrips);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
--- OCTranspoApp/public/js/client_map.js
+++ OCTranspoApp/public/js/client_map.js
@@ -1,23 +1,28 @@
-var map;
+var map = (function () {
+    var map = {};
 
-// Initialize to a view of Ottawa in general
-function initialize() {
-    var mapOptions = {
-        zoom: 15,
-        center: new google.maps.LatLng(45.415804, -75.700607),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+    map.map_canvas = null;
 
-    map = new google.maps.Map(document.getElementById('map_canvas'),
-                              mapOptions);
+    // Initialize to a view of Ottawa in general
+    map.initialize = function () {
+        var mapOptions = {
+            zoom: 15,
+            center: new google.maps.LatLng(45.415804, -75.700607),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
 
-    for (var i = 0, j = stops.length; i < j; ++i) {
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(stops[i]["stop_lat"], stops[i]["stop_lon"]),
-            title: stops[i]["stop_name"]
-        });
-        marker.setMap(map);
+        this.map_canvas = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+        for (var i = 0, j = stops.length; i < j; ++i) {
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(stops[i]["stop_lat"], stops[i]["stop_lon"]),
+                title: stops[i]["stop_name"],
+                map: this.map_canvas
+            });
+        }
     }
-}
 
-google.maps.event.addDomListener(window, 'load', initialize);
+    return map;
+}());
+
+google.maps.event.addDomListener(window, 'load', map.initialize);
